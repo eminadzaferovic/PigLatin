f26315db4a640628d1b198e51bae9b024436b920
Emina <emina.dzaferovic.95@gmail.com>
2017-02-10 13:13:55 +0100

    Add files via upload

---
+++ pig_latin.py
@@ -0,0 +1,9 @@
+word=input("Type the word to be converted: ")
+
+num_of_char=len(word)
+first_letter=word[0]
+last_string="ay"
+
+result=word[1:num_of_char]+first_letter+last_string
+
+print (result)
